b86b0a05c9b74680ded318bdfa3c74016fdf34b0
Giahao <Admin@LAPTOP-1JGLQK4M>
2024-04-04 21:53:13 +0700

    gia hao

---
+++ HPCakes/Areas/admin/adminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HPCakes.Areas.admin
+{
+    public class adminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "admin_default",
+                "admin/{controller}/{action}/{id}",
+                new { controller = "Shared",action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
