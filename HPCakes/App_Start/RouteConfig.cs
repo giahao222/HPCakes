5fa12660cfd48e697e64a9b281c91bd8a4fd857b
Giahao <Admin@LAPTOP-1JGLQK4M>
2024-04-04 20:06:48 +0700

    gia hao

--- HPCakes/App_Start/RouteConfig.cs
+++ HPCakes/App_Start/RouteConfig.cs
@@ -14,6 +14,105 @@ namespace HPCakes
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "Product",
+                "{type}/{meta}",
+                new { controller = "Product", action = "Product", meta = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "type","san-pham"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                "ProductDetail",
+                "{type}/{meta}/{id}",
+                new { controller = "Product", action = "ProductDetail", id = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "type","san-pham"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                "Blogss",
+                "{type}/{meta}",
+                new { controller = "Blog", action = "Blogs", meta = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "type","tin-tuc"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                "BlogsDetail",
+                "{type}/{meta}/{id}",
+                new { controller = "Blog", action = "BlogsDetail", id = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "type","tin-tuc"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                "About Us",
+                "{type}/{meta}",
+                new { controller = "AboutUs", action = "AboutUs", meta = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "type","thong-tin"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                "Contact Us",
+                "{type}/{meta}",
+                new { controller = "ContactUs", action = "ContactUs", meta = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "type","lien-he"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                "Home",
+                "{type}/{meta}",
+                new { controller = "Home", action = "Index", meta = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "type","trang-chu"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                "Index_getProduct",
+                "{controller}/{type}/{meta}/{id}",
+                new { controller = "Product", action = "ProductDetail", meta = UrlParameter.Optional, id = UrlParameter.Optional },
+                new RouteValueDictionary {
+                    {
+                        "controller","Home"
+                    },
+                    {
+                        "type","san-pham"
+                    }
+                },
+                new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ HPCakes/Controllers/BlogController.cs
@@ -0,0 +1,51 @@
+﻿using HPCakes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace HPCakes.Controllers
+{
+    public class BlogController : Controller
+    {
+        cakeshopEntities _db = new cakeshopEntities();
+        // GET: News
+        public ActionResult Blogs()
+        {
+            return View();
+        }
+
+        public ActionResult BlogsDetail(long id)
+        {
+            var v = from t in _db.news
+                    where t.id == id
+                    select t;
+            return View(v.FirstOrDefault());
+        }
+
+        /* public ActionResult getNews()
+         {
+
+             var v = from t in _db.news
+                     where t.hide == true
+                     orderby t.order descending
+                     select t;
+             return PartialView(v.ToList());
+         }*/
+        public ActionResult getBlogs(int? page)
+        {
+            ViewBag.meta = "tin-tuc";
+            int pageSize = 2; // số sản phẩm trên mỗi trang
+            int pageNumber = (page ?? 1);
+            var news = (from t in _db.news
+                        where t.hide == true
+                        orderby t.order descending
+                        select t).ToList();
+            IPagedList<news> pagedProducts = news.ToPagedList(pageNumber, pageSize);
+            return PartialView(pagedProducts);
+        }
+    }
+}
