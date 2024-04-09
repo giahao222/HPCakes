79f207d571f855635118dcff0eb00d6abb6788bb
Giahao <Admin@LAPTOP-1JGLQK4M>
2024-04-09 12:45:28 +0700

    gia hao

--- HPCakes/Areas/admin/Controllers/productsController.cs
+++ HPCakes/Areas/admin/Controllers/productsController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using HPCakes.Help;
 using HPCakes.Models;
 
 namespace HPCakes.Areas.admin.Controllers
@@ -14,12 +17,18 @@ namespace HPCakes.Areas.admin.Controllers
     {
         private cakeshopEntities db = new cakeshopEntities();
 
-        // GET: admin/products
-        public ActionResult Index()
+        // GET: admin/categories
+        public ActionResult Index(long? id = null)
         {
-            return View(db.products.ToList());
+            getCategory(id);
+            //return View(db.products.ToList());
+            return View();
         }
-
+        public void getCategory(long? selectedId = null)
+        {
+            ViewBag.Category = new SelectList(db.categories.Where(x => x.hide == true)
+                .OrderBy(x => x.order), "id", "name", selectedId);
+        }
         public ActionResult getProduct(long? id)
         {
             if (id == null)
@@ -31,7 +40,8 @@ namespace HPCakes.Areas.admin.Controllers
             return PartialView(m);
         }
 
-        // GET: admin/products/Details/5
+
+        // GET: admin/categories/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -46,30 +56,59 @@ namespace HPCakes.Areas.admin.Controllers
             return View(product);
         }
 
-        // GET: admin/products/Create
+        // GET: admin/categories/Create
         public ActionResult Create()
         {
+            getCategory();
             return View();
         }
 
-        // POST: admin/products/Create
+        // POST: admin/categories/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,categoryid,name,price,img,description,size,meta,hide,order,datebegin")] product product)
+        public ActionResult Create([Bind(Include = "id,name,price,img,description,meta,size,hdie,order,datebegin,categoryid")] product product, HttpPostedFileBase img)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var path = "";
+                var filename = "";
+                if (ModelState.IsValid)
+                {
+                    if (img != null)
+                    {
+                        //filename = Guid.NewGuid().ToString() + img.FileName;
+                        filename = DateTime.Now.ToString("dd-MM-yy-hh-mm-ss-") + img.FileName;
+                        path = Path.Combine(Server.MapPath("~/Content/upload/img/product"), filename);
+                        img.SaveAs(path);
+                        product.img = filename; //Lưu ý
+                    }
+                    else
+                    {
+                        product.img = "logo.png";
+                    }
+                    product.datebegin = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                    product.meta = Functions.ConvertToUnSign(product.meta); //convert Tiếng Việt không dấu
+                    product.order = getMaxOrder(product.categoryid);
+                    db.products.Add(product);
+                    db.SaveChanges();
+                    //return RedirectToAction("Index");
+                    return RedirectToAction("Index", "product", new { id = product.categoryid });
+                }
             }
-
+            catch (DbEntityValidationException e)
+            {
+                throw e;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             return View(product);
         }
 
-        // GET: admin/products/Edit/5
+        // GET: admin/categories/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -81,26 +120,60 @@ namespace HPCakes.Areas.admin.Controllers
             {
                 return HttpNotFound();
             }
+
+            getCategory(product.categoryid);
             return View(product);
         }
 
-        // POST: admin/products/Edit/5
+        // POST: admin/categories/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,categoryid,name,price,img,description,size,meta,hide,order,datebegin")] product product)
+        public ActionResult Edit([Bind(Include = "id,name,price,img,description,meta,size,hdie,order,datebegin,categoryid")] product product, HttpPostedFileBase img)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var path = "";
+                var filename = "";
+                product temp = db.products.Find(product.id);
+                if (ModelState.IsValid)
+                {
+                    if (img != null)
+                    {
+                        //filename = Guid.NewGuid().ToString() + img.FileName;
+                        filename = DateTime.Now.ToString("dd-MM-yy-hh-mm-ss-") + img.FileName;
+                        path = Path.Combine(Server.MapPath("~/Content/upload/img/product"), filename);
+                        img.SaveAs(path);
+                        temp.img = filename; //Lưu ý
+                    }
+                    // temp.datebegin = Convert.ToDateTime(DateTime.Now.ToShortDateString());                   
+                    temp.name = product.name;
+                    temp.price = product.price;
+                    temp.description = product.description;
+                    temp.meta = Functions.ConvertToUnSign(product.meta); //convert Tiếng Việt không dấu
+                    temp.size = product.size;
+                    temp.hide = product.hide;
+                    temp.order = product.order;
+                    db.Entry(temp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    //return RedirectToAction("Index");
+                    return RedirectToAction("Index", "product", new { id = product.categoryid });
+                }
             }
+            catch (DbEntityValidationException e)
+            {
+                throw e;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
             return View(product);
         }
 
-        // GET: admin/products/Delete/5
+        // GET: admin/categories/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -115,7 +188,7 @@ namespace HPCakes.Areas.admin.Controllers
             return View(product);
         }
 
-        // POST: admin/products/Delete/5
+        // POST: admin/categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -133,6 +206,13 @@ namespace HPCakes.Areas.admin.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public int getMaxOrder(long? CategoryId)
+        {
+            if (CategoryId == null)
+                return 1;
+            return db.products.Where(x => x.categoryid == CategoryId).Count();
         }
     }
 }
